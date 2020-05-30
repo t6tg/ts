814eb2836bd6d77ff29f34e2ec9217d86b0546fa
thanawatgulati <thanawatgulatijames@gmail.com>
2020-05-30 12:09:12 +0700

    🅳 DataTypes

---
+++ dist/DataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+var isDone = true;
+//----------------------------------
+var decimal = 10;
+var hex = 0xf00d;
+var binary = 6;
+var octa = 23358;
+var age = 19;
+// --------------------------------
+var myName = "James";
+var myProfile = "I'm " + myName + " , I'm " + age + " year olds.";
+//---------------------------------
+console.log(myProfile);
---
+++ src/DataTypes.ts
@@ -0,0 +1,12 @@
+const isDone: boolean = true;
+//----------------------------------
+const decimal: number = 10;
+const hex: number = 0xf00d;
+const binary: number = 0b00110;
+const octa: number = 0o55476;
+const age: number = 19;
+// --------------------------------
+const myName: string = "James";
+const myProfile: string = `I'm ${myName} , I'm ${age} year olds.`;
+//---------------------------------
+console.log(myProfile);
