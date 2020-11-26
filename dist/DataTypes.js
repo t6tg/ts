f45327bbbcf5dc0ebae546f7b8570b472830f43a
thanawatgulati <thanawatgulatijames@gmail.com>
2020-11-27 02:38:40 +0700

    End of DataType

--- dist/DataTypes.js
+++ dist/DataTypes.js
@@ -12,4 +12,22 @@ var myProfile = "I'm " + myName + " , I'm " + age + " year olds.";
 //---------------------------------
 var list = [1, 2, 3];
 var numList = [1, 2, 3, 4, 5];
-numList.forEach(function (n) { return console.log(n); });
+// numList.forEach((n) => console.log(n));
+//---------------------------------
+var tuple = ["James", 2];
+//---------------------------------
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Black"] = 1] = "Black";
+    Color[Color["Pink"] = 2] = "Pink";
+})(Color || (Color = {}));
+var color = Color[1];
+//---------------------------------
+var notSure = 4;
+// create({ name: "James" });
+// create("0");
+//---------------------------------
+var someValue = "Hello from the other side.";
+var strLength = someValue.length;
+console.log(strLength);
--- src/DataTypes.ts
+++ src/DataTypes.ts
@@ -11,6 +11,24 @@ const myProfile: string = `I'm ${myName} , I'm ${age} year olds.`;
 //---------------------------------
 const list: number[] = [1, 2, 3];
 const numList: Array<number> = [1, 2, 3, 4, 5];
-//----------------------------------
-
-numList.forEach((n) => console.log(n));
+// numList.forEach((n) => console.log(n));
+//---------------------------------
+const tuple: [string, number] = ["James", 2];
+//---------------------------------
+enum Color {
+  Red,
+  Black,
+  Pink,
+}
+let color: string = Color[1];
+//---------------------------------
+let notSure: unknown = 4;
+//---------------------------------
+declare function create(o: object | null): void;
+// create({ name: "James" });
+// create("0");
+//---------------------------------
+let someValue: string = "Hello from the other side.";
+let strLength: number = (someValue as string).length;
+// console.log(strLength);
+//---------------------------------
